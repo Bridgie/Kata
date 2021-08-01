7044dc1834fcec1aa5054223a4fac804e251eca0
Malkhaz Julakidze <datoj81@gmail.com>
2021-08-01 20:45:15 +0400

    added initial structure

--- src/main/java/Kata/Classes/Post.java
+++ src/main/java/Kata/Classes/Post.java
@@ -1,4 +1,17 @@
 package Kata.Classes;
 
+import java.util.Date;
+
 public class Post {
+    public int Likes;
+    public int Views;
+    public User Author;
+    public Date PostedDate;
+    public String PostDetail;
+
+    public Post(User author, Date postedDate, String postDetail) {
+        Author = author;
+        PostedDate = postedDate;
+        PostDetail = postDetail;
+    }
 }
